09f4edc46b66013b97a32ee5fc947159c9526c9a
kimhart <kim@jwplayer.com>
2020-03-29 12:44:22 -0400

    remove apollo on home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { useQuery } from "@apollo/react-hooks"
-import gql from "graphql-tag"
+// import { useQuery } from "@apollo/react-hooks"
+// import gql from "graphql-tag"
 import "../styles/main.less"
 
 export const rackAPI = graphql`
@@ -15,32 +15,32 @@ export const rackAPI = graphql`
 `
 
 // This query is executed at run time by Apollo.
-const GET_REPORTS = gql`
-  {
-    suspiciousActivityReport {
-      id
-      suspicion_type
-      date_observed
-      room_number
-    }
-  }
-`
+// const GET_REPORTS = gql`
+//   {
+//     suspiciousActivityReport {
+//       id
+//       suspicion_type
+//       date_observed
+//       room_number
+//     }
+//   }
+// `
 
 export default ({
   data: {
     rackAPI: { victims },
   },
 }) => {
-  const { data } = useQuery(GET_REPORTS);
+  // const { data } = useQuery(GET_REPORTS);
   return (
     <div className="rack-app">
       <h1>Victims</h1>
       {victims?.map((victim, i) => (
         <h3 key={i}>{victim.name}</h3>
       ))}
-      {data?.suspiciousActivityReport?.map((report, i) => (
+      {/* {data?.suspiciousActivityReport?.map((report, i) => (
         <div key={i}>{report.suspicion_type}</div>
-      ))}
+      ))} */}
     </div>
   )
 }
