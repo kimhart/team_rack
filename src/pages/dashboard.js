2e5b9f5f54d8ee71285646d88160e9320243ab1b
kimhart <kim@jwplayer.com>
2020-03-29 13:53:06 -0400

    homepage

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -18,26 +18,31 @@ export default class Form extends React.Component {
       }
     `
     return (
-      <Query query={MATCH_QUERY}>
-        {({ loading, error, data }) => {
-          if (loading) return "Loading..."
-          if (error) return `Error! ${error.message}`
+      <div className="rack-app__dash">
+        <h1>Facial Recognition Matches</h1>
+        <Query query={MATCH_QUERY}>
+          {({ loading, error, data }) => {
+            if (loading) return "Loading..."
+            if (error) return `Error! ${error.message}`
 
-          const { matchResults } = data
-          return matchResults?.map(match => (
-            <div>
-              {match.similarity_bucket}:
-              <img
-                src={`https://${match.input_bucket}.s3.amazonaws.com/${match.input_prefix}/${match.input_filename}`}
-              />
-              match:
-              <img
-                src={`https://${match.similarity_bucket}.s3.amazonaws.com/${match.similarity_prefix}/${match.similarity_filename}`}
-              />
-            </div>
-          ))
-        }}
-      </Query>
+            const { matchResults } = data
+            return matchResults?.map(match => (
+              <div className="rack-app__matches">
+                <h2>{match.similarity_bucket}:</h2>
+                <div className="rack-app__matches-images">
+                  <img
+                    src={`https://${match.input_bucket}.s3.amazonaws.com/${match.input_prefix}/${match.input_filename}`}
+                  />
+                  <h4>{(match.similarity_score * 100).toFixed(2)}% similarity score</h4>
+                  <img
+                    src={`https://${match.similarity_bucket}.s3.amazonaws.com/${match.similarity_prefix}/${match.similarity_filename}`}
+                  />
+                </div>
+              </div>
+            ))
+          }}
+        </Query>
+      </div>
     )
   }
 }
